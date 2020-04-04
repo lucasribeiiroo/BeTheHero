8b2afc3cf62b956a5f5b0437d18ccecdaf066238
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2020-04-04 18:07:32 -0300

    adicao de dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import Logon from './pages/Logon';
+import React, { Component } from 'react';
 import Routes from './routes';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import './global.css'
 
-function App() {
-  return (
-    <Routes />
-  )
+class App extends Component {
+  state ={
+    darkMode: false,
+    modeLabel: 'Dark Mode',
+  }
+
+  handleTheme = () => {
+    const { darkMode } = this.state;
+    const newLabel = darkMode? 'Dark Mode': 'White Mode';
+    this.setState((prevState) => ({
+      darkMode: !prevState.darkMode,
+      modeLabel: newLabel,
+    }));
+  }
+
+  render(){
+    const { darkMode, modeLabel } = this.state;
+    console.log(darkMode);
+    
+    const appTheme = darkMode ? 'dark-mode': 'white-mode';
+    return (
+      <div className={appTheme}>
+        <nav className={`nav-bar ${appTheme} `}>
+          <div className={`nav-content ${appTheme}`}>
+          <FormControlLabel
+            control={<Switch onChange={this.handleTheme}/>}
+            label={modeLabel}
+          />
+          </div>
+        </nav>
+        <Routes />
+      </div>
+    )
+  }
 }
 
 export default App;
