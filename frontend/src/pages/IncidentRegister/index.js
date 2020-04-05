addd98217d21a457edec867b6d2be7511af175ef
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2020-04-05 20:55:16 -0300

    feat: adicao de pagina de cadastro de incidente

---
+++ frontend/src/pages/IncidentRegister/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import './styles.css';
+import logo from '../../assets/logo.svg'
+import './styles.css';
+
+export default function IncidentRegister(){
+  return (
+    <div className="incident-register-container">
+      <div className="content">
+        <section>
+        <img src={logo} alt="logo-bth"/>
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+        
+          <Link className='back-link' to="/profile">
+            <FiArrowLeft size={16} color="#E02041"/>
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título"/>
+          <textarea placeholder="Descrição"/>
+          <input placeholder="Valor em reais"/>
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Register from './pages/Register'
 import Logon from './pages/Logon';
 import Profile from './pages/Profile';
+import IncidentRegister from './pages/IncidentRegister';
 
 export default function Routes(){
   return(
@@ -11,6 +12,7 @@ export default function Routes(){
         <Route exact path="/" component={Logon}></Route>
         <Route path="/register" component={Register}></Route>
         <Route path="/profile" component={Profile}></Route>
+        <Route path="/incidents/new" component={IncidentRegister}></Route>
       </Switch>
     </BrowserRouter>
   )
