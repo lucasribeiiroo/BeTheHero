061d9eea26619c39c3bd406b0f195ce8f076a122
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2020-04-05 19:48:34 -0300

    feat: Adicao de pagina profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Register from './pages/Register'
 import Logon from './pages/Logon';
+import Profile from './pages/Profile';
 
 export default function Routes(){
   return(
@@ -9,6 +10,7 @@ export default function Routes(){
       <Switch>
         <Route exact path="/" component={Logon}></Route>
         <Route path="/register" component={Register}></Route>
+        <Route path="/profile" component={Profile}></Route>
       </Switch>
     </BrowserRouter>
   )
