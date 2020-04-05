fa4f9dbd74db481e1d0c030c12e0dc519f9453fe
Lucas Ribeiro <lucasribeiro@MacBook-Pro-de-Lucas.local>
2020-04-05 16:50:16 -0300

    feat: Criado pagina para cadastro

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Register from './pages/Register'
 import Logon from './pages/Logon';
 
 export default function Routes(){
   return(
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Logon}></Route>
+        <Route exact path="/" component={Logon}></Route>
+        <Route path="/register" component={Register}></Route>
       </Switch>
     </BrowserRouter>
   )
